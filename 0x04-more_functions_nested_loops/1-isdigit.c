1fa8e23f92fb8203ad3dab000820a67233f6e68d
charlessnow <ekekwecharles50@gmail.com>
2023-02-23 06:13:06 -0800

    update _isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
+
 /**
- * _isdigit - checks for lowercase
- * @c: First operand
- * Return: 1 or 0
+ * _isdigit - checks for digithood
+ * @c: character to checked for digit membership
+ *
+ * Return: 1 if digit, 0 if not digit
  */
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
-
 	return (0);
 }
