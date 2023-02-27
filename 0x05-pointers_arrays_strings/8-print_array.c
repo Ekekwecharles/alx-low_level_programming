8b57cd4f3055bffeab6bb0fc6d5e8df9033a5767
charlessnow <ekekwecharles50@gmail.com>
2023-02-27 14:06:52 -0800

    print n no of int

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,17 +2,22 @@
 #include <stdio.h>
 /**
 * print_array -> print in 2s
-* @str: the string to be printed in rev
+* @a: the string to be printed in rev
+* @n: Second par
 */
 void print_array(int *a, int n)
 {
 	int index = 0;
 
-	for (index = 0; index < n; index++)
+	if (n > 0)
 	{
-		if (index == n - 1)
-			printf("%d\n", a[index]);
-		else
-			printf("%d, ", a[index]);
-	}
+		for (index = 0; index < n; index++)
+		{
+			if (index == n - 1)
+				printf("%d\n", a[index]);
+			else
+				printf("%d, ", a[index]);
+		}
+	} else
+		printf("\n");
 }
