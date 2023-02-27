f9c902f9e1f0dfbb5e5845cb00e8d86e69a4eced
charlessnow <ekekwecharles50@gmail.com>
2023-02-27 08:57:39 -0800

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- *_puts - swap two integers
+ * _puts - swap two integers
  * @str: First operand
  *
  */
 void _puts(char *str)
 {
-	_putchar(*str);
+	while (*str)
+		_putchar(*str++);
+
 	_putchar('\n');
 }
