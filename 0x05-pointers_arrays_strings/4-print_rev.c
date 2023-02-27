9bd3c41f9383eef2f2aa9b04b3d9d02f5ccb549a
charlessnow <ekekwecharles50@gmail.com>
2023-02-27 10:05:11 -0800

    reverse String

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_rev - reverse string
- * @s: First operand
- *
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void print_rev(char *s)
 {
