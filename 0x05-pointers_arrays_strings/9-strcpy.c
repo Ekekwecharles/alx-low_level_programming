d09b3643930e3f4fb99ef3521d67a70e1804768f
charlessnow <ekekwecharles50@gmail.com>
2023-02-27 14:39:47 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,10 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int a;
 
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (dest);
 }
