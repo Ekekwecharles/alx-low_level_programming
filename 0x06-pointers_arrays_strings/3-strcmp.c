638c47afaff890062ff5bfdc013bccf980c5e0cb
charlessnow <ekekwecharles50@gmail.com>
2023-03-01 10:59:03 -0800

    More pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 /**
  * _strcmp - compares string
  * @s1: Firest operand
- * @s2: second operand 
+ * @s2: second operand
  * Return: 0
  */
 
@@ -16,14 +16,3 @@ int _strcmp(char *s1, char *s2)
 
 	return (*s1 - *s2);
 }
-
-
-/**{
-	if (*s1 > *s2)
-		return (15);
-	else if (*s2 > *s1)
-		return (-15);
-	else
-		return (0);
-}
-*/
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n)
 {
 	int *start = a;
 	int *end = a + n - 1;
-	
+
 	while (start < end)
 	{
 		int temp = *start;
