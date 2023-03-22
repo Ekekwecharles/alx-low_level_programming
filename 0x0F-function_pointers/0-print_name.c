0f9a5d519e22118dd5d20dfa09e1c774d9b9fbc6
charlessnow <ekekwecharles50@gmail.com>
2023-03-22 11:13:05 -0700

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
- * @name: First parameter
- * @f: function pointer
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Prints the result of simple operations.
@@ -9,42 +9,35 @@
  *
  * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*opfunc_ptr)(int, int);
-
-	int result = 0, num1 = 0, num2 = 0;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' &&
-			argv[2][0] != '-' &&
-			argv[2][0] != '*' &&
-			argv[2][0] != '/' &&
-			argv[2][0] != '%')
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' && argv[3][0] == '0') ||
-			(argv[2][0] == '%' && argv[3][0] == '0'))
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	opfunc_ptr = get_op_func(argv[2]);
-
-	result = (*opfunc_ptr)(num1, num2);
-	printf("%d\n", result);
-
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
