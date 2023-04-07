f326b18a50bc7424bd2acf88081405c7e43402ba
Charlessnow <ekekwecharles50@gmail.com>
2023-04-07 07:55:50 -0700

    reads a text file and prints it to the stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * read_textfile - reads a text file and prints it
  *		to the POSIX standard output.
  * @filename: First parameter
@@ -17,20 +17,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
 	buffer = malloc(sizeof(char) * letters);
+
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
+
+	fd = open(filename, O_RDONLY);
+		if (fd == -1)
+			return (0);
 
 	while ((rd = read(fd, buffer + count, letters - count)) > 0)
 	{
 		count += rd;
 	}
+
 	if (rd == -1)
 	{
 		free(buffer);
