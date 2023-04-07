53d702d24e95c4fec4af578a0e3a8e28d866a020
Charlessnow <ekekwecharles50@gmail.com>
2023-04-07 07:57:50 -0700

    fix betty issue

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-
 	if (buffer == NULL)
 		return (0);
 
@@ -27,9 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			return (0);
 
 	while ((rd = read(fd, buffer + count, letters - count)) > 0)
-	{
 		count += rd;
-	}
 
 	if (rd == -1)
 	{
