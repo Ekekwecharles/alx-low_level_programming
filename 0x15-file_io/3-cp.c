ffac986e83320cdb1c552e3fc69d5a405a7ee650
Charlessnow <ekekwecharles50@gmail.com>
2023-04-07 22:51:00 -0700

    copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+
+/**
+ * create_buffer_and_copy - copies a file to another
+ * @buffer: First parameter
+ * @len: Second parameter
+ * @fd1: third
+ * @fd2: fourth
+ *
+ * Return: integer
+ */
+
+int create_buffer_and_copy(char **buffer, int len, int fd1, int fd2)
+{
+	int rd, wr;
+
+	*buffer = malloc(sizeof(char) * len);
+	if (*buffer == NULL)
+	{
+		close(fd1);
+		close(fd2);
+		return (-3);
+	}
+
+	while ((rd = read(fd1, *buffer, 1024)) > 0)
+	{
+		wr = write(fd2, *buffer, rd);
+		if (wr != rd)
+		{
+			free(*buffer);
+			close(fd1);
+			close(fd2);
+			return (-3);
+		}
+	}
+	if (rd == -1)
+	{
+		free(*buffer);
+		close(fd1);
+		close(fd2);
+		return (-2);
+	}
+
+	return (1);
+}
 
 /**
  * copy_file - copies a file to another
@@ -14,32 +57,25 @@
 
 int copy_file(char *from, char *to)
 {
-	int fd1, fd2, cls1, cls2, len, wr, rd, count = 0;
-	char *buffer;
-
-	len = strlen(from);
-	buffer = malloc(sizeof(char) * len);
-	if (buffer == NULL)
-		return (-3);
+	int fd1, fd2, cls1, cls2, len, ret;
+	char *buffer = NULL;
 
 	fd1 = open(from, O_RDONLY);
-	fd2 = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	while ((rd = read(fd1, buffer, 1024)) > 0)
-		count += rd;
-	if (rd == -1 || fd1 == -1)
-	{
-		free(buffer);
+	if (fd1 == -1)
 		return (-2);
-	}
-	buffer[count] = '\0';
-
-	wr = write(fd2, buffer, count);
-	if (wr == -1 || fd2 == -1)
+	fd2 = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
 	{
-		free(buffer);
+		close(fd1);
 		return (-3);
 	}
+	len = lseek(fd1, 0, SEEK_END);
+	lseek(fd1, 0, SEEK_SET);
+
+	ret = create_buffer_and_copy(&buffer, len, fd1, fd2);
+
+	if (ret < 0)
+		return (ret);
 
 	free(buffer);
 
