7839a35ffa056996cbdb323d30167c4858db53e8
Charlessnow <ekekwecharles50@gmail.com>
2023-04-08 18:16:39 -0700

    get value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_bit -return value of a bit at a given index
+ * @n: First parametr
+ * @index: second
+ *
+ * Return: value at index if success
+ *		-1 if fail
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int count = 0, copy = n, i;
+	char *buffer;
+
+	while (copy > 0)
+	{
+		copy = copy / 2;
+		count++;
+	}
+
+	buffer = malloc(sizeof(int) * count);
+
+	if (buffer == NULL)
+		return (-1);
+
+	i = 0;
+	copy = n;
+	while (i < count)
+	{
+		buffer[i++] = (copy % 2);
+		copy = copy / 2;
+	}
+	if (index >= count)
+		return (-1);
+
+	return (buffer[index]);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -8,3 +8,5 @@ Tasks
 1. Write a function that prints the binary representation of a number.
 1-print_binary.c
 
+2. Write a function that returns the value of a bit at a given index
+2-get_bit.c
