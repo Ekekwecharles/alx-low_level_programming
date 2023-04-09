33ff7ad37306382bb716fac3d60c65e7b5f48592
Charlessnow <ekekwecharles50@gmail.com>
2023-04-09 00:38:39 -0700

    get value at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,36 +12,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count = 0, copy = n, i;
-	int *buffer;
-	int val;
+	unsigned int mask;
 
-	if (n == 0)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	while (copy > 0)
-	{
-		copy = copy / 2;
-		count++;
-	}
-
-	buffer = malloc(sizeof(int) * count);
-
-	if (buffer == NULL)
-		return (-1);
-
-	i = 0;
-	copy = n;
-	while (i < count)
-	{
-		buffer[i++] = (copy % 2);
-		copy = copy / 2;
-	}
-	if (index >= count)
-		return (-1);
-
-	val = buffer[index];
-	free(buffer);
-
-	return (val);
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
 }
