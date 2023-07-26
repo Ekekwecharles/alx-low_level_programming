870bf42540a9d555f628b7a212468832a7a0ee31
Charlessnow <ekekwecharles50@gmail.com>
2023-07-26 06:02:00 -0700

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Calculate the perimeter of the island"""
+
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in the grid.
@@ -9,26 +13,17 @@ def island_perimeter(grid):
     int: The perimeter of the island.
     """
 
-    if not grid or not grid[0]:
-        return 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    rows, cols = len(grid), len(grid[0])
-    perimeter = 0
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                # Count the number of surrounding water zones (0's)
-                water_zones = 0
-                if row - 1 >= 0 and grid[row - 1][col] == 0:
-                    water_zones += 1
-                if row + 1 < rows and grid[row + 1][col] == 0:
-                    water_zones += 1
-                if col - 1 >= 0 and grid[row][col - 1] == 0:
-                    water_zones += 1
-                if col + 1 < cols and grid[row][col + 1] == 0:
-                    water_zones += 1
-
-                perimeter += water_zones
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
